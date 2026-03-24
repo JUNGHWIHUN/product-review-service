3d40623960a7bd56f5666c0f6cf3a1697a8e1809
Jung HwiHun <hwihun5623@naver.com>
2026-03-24 13:16:38 +0900

    chore: pre-commit 설정 및 코드 포맷 적용

---
+++ backend/apps/accounts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.viewsets import ViewSet
+
+
+class UserViewSet(ViewSet):
+
+    def list(self, request):
+        pass
+
+    def retrieve(self, request, pk=None):
+        pass
---
+++ backend/apps/products/views.py
@@ -0,0 +1,22 @@
+from rest_framework.viewsets import ViewSet
+
+
+class ProductViewSet(ViewSet):
+    """
+    Product CRUD API
+    """
+
+    def list(self, request):
+        pass
+
+    def retrieve(self, request, pk=None):
+        pass
+
+    def create(self, request):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
